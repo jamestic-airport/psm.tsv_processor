87a42fa88b6010d46bc99e41c60ba48fcd13f90e
James R <j.ryu.rosse@gmail.com>
2023-07-08 18:42:25 +1000

    Summarises all PTM site counts from all output excel files into one sheet.

--- localisation summary.py
+++ localisation summary.py
@@ -1,19 +1,47 @@
 import pandas as pd
+import os
+import pprint
 
-excel_file = pd.ExcelFile('psm_output.xlsx')
-counts = {}
+def get_counts(file_path):
 
-for sheet_name in excel_file.sheet_names:
+    df_all = pd.DataFrame()
+    excel_file = pd.ExcelFile(file_path)
+    for sheet_name in excel_file.sheet_names:
+        df = excel_file.parse(sheet_name)
+        # mod_site_counts is a df containing the localisation site and its frequency
+        mod_site_counts = df['Full Localisation Sites'].value_counts().reset_index()     
+        mod_site_counts.columns = ['Value', 'Count']
+        mod_site_counts.insert(0, 'PTM', sheet_name)
+        mod_site_counts.insert(0, 'Dataset', file_path)
+        df_all = pd.concat([df_all, mod_site_counts], ignore_index=True)
+    return df_all
 
-    df = excel_file.parse(sheet_name)
 
-    # mod_site_counts is a df containing the localisation site and its frequency
-    mod_site_counts = df['Full Localisation Sites'].value_counts().reset_index()     
-    mod_site_counts.columns = ['Value', 'Count']
-    # counts is a dict of dfs. The keys are the modification types.
-    counts[sheet_name] = mod_site_counts     
+def get_excel_files():
 
-with pd.ExcelWriter('Localization Summary.xlsx') as writer:
+    excel_files = []
+    directory = os.path.join(os.getcwd(), 'output')
+    for filename in os.listdir(directory):
+        if filename.endswith(".xlsx") or filename.endswith(".xls"):  # Check if it's an Excel file
+            file_path = os.path.join(directory, filename)
+            excel_files.append(file_path)
+    return excel_files
 
-    for sheet_name, mod_site_counts in counts.items():
-        mod_site_counts.to_excel(writer, sheet_name=sheet_name, index=False)
+
+def create_file(df):
+    print(df)
+    df.to_excel('Localization Summary Test 1.xlsx', index=False)  # Specify the file name and remove index column
+
+
+# Iterate over each file in the directory
+
+all_counts = pd.DataFrame()
+excel_files = get_excel_files()
+for file in excel_files:
+    counts = get_counts(file)
+    all_counts = pd.concat([all_counts, counts], ignore_index=True)
+
+create_file(all_counts)
+
+
+
